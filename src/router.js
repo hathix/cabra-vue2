c3fbf3ab6ed1035291e1d6002309187a7e6f5d46
Neel Mehta <neelmehta96@gmail.com>
2018-06-29 15:03:41 -0400

    fetch deck

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import DeckPage from "./views/DeckPage.vue";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/decks/:id",
+      name: "deckpage",
+      component: DeckPage
     },
     {
       path: "/about",
---
+++ src/views/DeckPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    My id is {{ $route.params.id }}. My name is {{ deck.name }}.
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+// import DeckList from "@/components/DeckList.vue";
+
+import _ from "lodash";
+
+export default {
+  name: "deckpage",
+  computed: {
+    deck() {
+      // get deck from list of decks
+      let deckId = this.$route.params.id;
+      // TODO search in list of decks for this
+      let allDecks = this.$store.state.decks;
+      // find first deck that matches
+      return _.find(allDecks, { id: deckId });
+    }
+  }
+  // components: {
+  //   HelloWorld,
+  //   DeckList
+  // }
+};
+</script>
+
+<style scoped lang="scss">
+  // .md-speed-dial {
+  //   z-index: 100;
+  // }
+</style>
