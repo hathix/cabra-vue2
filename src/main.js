7737842fd56e78c3430241eb6e841e91a60d02c6
Neel Mehta <neelmehta96@gmail.com>
2018-06-29 12:47:37 -0400

    lint

--- src/main.js
+++ src/main.js
@@ -5,19 +5,18 @@ import store from "./store";
 
 /* VUE MATERIAL */
 // import Vue from 'vue'
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css' // Choose a theme https://vuematerial.io/themes/prebuilt
+import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css"; // Choose a theme https://vuematerial.io/themes/prebuilt
 
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
 /* END VUE MATERIAL */
-
 
 Vue.config.productionTip = false;
 
-let app = new Vue({
+new Vue({
   router,
   store,
   render: h => h(App)
