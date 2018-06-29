71e516eb49f21efbcb8c9633f82d8467e2b0d7eb
Neel Mehta <neelmehta96@gmail.com>
2018-06-29 11:16:47 -0400

    add material

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <md-button class="md-raised md-accent">
+      This is a Material button
+    </md-button>
+
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+/* VUE MATERIAL */
+// import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css' // Choose a theme https://vuematerial.io/themes/prebuilt
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+/* END VUE MATERIAL */
+
+
 Vue.config.productionTip = false;
 
-new Vue({
+let app = new Vue({
   router,
   store,
   render: h => h(App)
