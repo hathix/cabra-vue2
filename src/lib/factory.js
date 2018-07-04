80da6e0e8afd9aabbfb0ad9121c7fa787363bdb9
Neel Mehta <neelmehta96@gmail.com>
2018-07-04 15:06:38 -0400

    better ids

--- src/lib/factory.js
+++ src/lib/factory.js
@@ -1,22 +1,24 @@
 // Factory for creating cards + decks
 
 import * as Rank from "@/lib/rank";
+import randomize from "randomatic";
 
 let factory = {};
 
 factory.createDeck = function({ name }) {
   return {
     name: name,
-    id: "d" + Date.now(), // some unique string
+    id: randomize('Aa0', 16), // random id string for card
     cards: [] // no cards yet
   };
 };
 
 factory.createCard = function({ question, answer }) {
+
   return {
     question: question,
     answer: answer,
-    id: "c" + Date.now(), // some unique string
+    id: randomize('Aa0', 16), // random id string for card
     rankName: Rank.BASE_RANK.name, // base rank
     repsLeft: Rank.BASE_RANK.baseRepsLeft
   };
